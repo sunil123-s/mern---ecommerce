993aee3b09359eec9d946fef225df601863d94cf
Sunil kumar <sunil1234kumar567@gmail.com>
2025-01-04 20:53:45 +0530

    adding favicon

--- frontend/src/pages/common/SkeletonCard.jsx
+++ frontend/src/pages/common/SkeletonCard.jsx
@@ -2,7 +2,7 @@
 const SkeletonCard = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
-      <div className="w-12 h-12 border-4 border-t-black-500 border-black-200 rounded-full animate-spin"></div>
+      <div className="w-12 h-12 border-4 border-t-gray-500 border-gary-500 rounded-full animate-spin"></div>
     </div>
   );
 }
