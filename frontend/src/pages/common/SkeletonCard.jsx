57f5e3a8c8b3e6071e70927824629a6e8640e10a
Sunil kumar <sunil1234kumar567@gmail.com>
2025-01-04 20:38:06 +0530

    adding spinner

--- frontend/src/pages/common/SkeletonCard.jsx
+++ frontend/src/pages/common/SkeletonCard.jsx
@@ -2,7 +2,7 @@
 const SkeletonCard = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
-      <div className="w-12 h-12 border-4 border-t-blue-500 border-blue-200 rounded-full animate-spin"></div>
+      <div className="w-12 h-12 border-4 border-t-black-500 border-black-200 rounded-full animate-spin"></div>
     </div>
   );
 }
